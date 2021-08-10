92b447dcc9b202fa66e69cd6f9be9361fe6d588d
Observatorio de Política y redes sociales <67342656+Politica-y-redes-sociales@users.noreply.github.com>
2021-08-09 21:42:22 -0400

    Add files via upload

---
+++ botDetector.R
@@ -0,0 +1,115 @@
+#install.packages('plyr')
+library(plyr)
+library(readr)
+library(dplyr)#manejo de ficheros
+library(purrr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+library(scales)
+library(tidytext)
+library(tm)
+library(colorspace)
+library(sqldf)
+library(rtweet)
+library(curl)
+library(gsubfn)
+library(proto)
+library(RSQLite)
+
+token <- create_token ( 
+  app<-"Servidor 45", 
+  consumer_key<-"zims51dZYe7BpC1AnVl5NTdFm", 
+  consumer_secret<-"xvcIpOB2g1b7xaB9K7E0QVR4OGUHM2PzoVbOmEXbw9jMXfLj5q",
+  access_token<-"987408666319118336-slXOkbUWQCEMxHRiahPJcqlbK4zjk5Z", 
+  access_secret<-"LdOJGJHyuqfiqX4H2k9iCbb9bycb9aYxdjtfMMy4egKkA"
+)
+
+carpeta = "/Users/alfonsoopazom/Desktop/Observatorio/ProgramasenR/DetectorBot"
+carpeta_base = paste(carpeta,"Bases",sep="/")
+cuentas = dir(carpeta_base)
+cuentas = as.data.frame(cuentas)
+
+if(dir.exists(paste(carpeta, "Resultados", sep = "/")))
+{}else
+{dir.create(paste(carpeta, "Resultados", sep = "/"))}
+
+numArchivos = nrow(cuentas)
+i=1
+
+while(i <= numArchivos)
+{
+  nombre = substr(toString(cuentas$cuentas),1,(str_length(cuentas$cuentas)-4))
+  archivo_temporal = paste(carpeta_base,toString(cuentas$cuentas),sep="/")
+  CarpetaResultados = paste(carpeta,"Resultados",sep = "/")
+  consulta = read.csv(archivo_temporal,header = TRUE,sep = ",",encoding = "UTF-8")
+  
+  #Cantidad de filas de la base
+  total_filas <- sqldf("SELECT count(DISTINCT(screen_name)) total_filas FROM consulta")
+  #Cuenta del usuario
+  cuenta<- sqldf("SELECT DISTINCT(screen_name) Cuentas, screen_name numeros, name FROM consulta")
+  cuenta$numeros<-gsub('[a-z]|[A-Z]|_|-','',cuenta$numeros)
+  cuenta$numeros<-gsub('[a-z]|[A-Z]|_|-','',cuenta$numeros)
+  
+  #---- NUMEROS EN EL NOMBRE DE CUENTA ----#
+  cuenta <- sqldf("SELECT Cuentas, numeros, length(Cuentas) Cantidad, length(numeros) Cantidad_num, 
+                   round(((cast(length(numeros) as float))/(cast(length(Cuentas) as float)))*100,2) Porcentaje
+                   FROM cuenta
+                   WHERE Cantidad_num >0 AND Porcentaje > 50.00
+                   ORDER BY Cantidad_num
+                   DESC")
+  contarNum <-sqldf("SELECT count(Cuentas) Cantidad_n FROM cuenta")
+  
+  #---- RETWEET ----#
+  retweet <- sqldf("SELECT DISTINCT(screen_name) cuenta, text, is_retweet, retweet_screen_name
+                  FROM consulta WHERE is_retweet='1'
+                  GROUP BY cuenta")
+  contarRetweet <- sqldf("SELECT count(cuenta) Cantidad_Re FROM retweet")
+  
+  #---- DESCRIPCION ----#
+  descripcion <- sqldf("SELECT DISTINCT(screen_name) cuenta, description Descripcion
+                       FROM consulta WHERE Descripcion =''")
+  contarDesc <- sqldf("SELECT count(cuenta) Cantidad_Des FROM descripcion")
+  
+  #---- LOCALIZACION ----#
+  localizacion <- sqldf("SELECT DISTINCT(screen_name) cuenta, location Localizacion
+                       FROM consulta WHERE location =''")
+  contarLoc <- sqldf("SELECT count(cuenta) Cantidad_Loca FROM localizacion")
+  
+  porcentaje_num <- round((contarNum$Cantidad_n/total_filas$total_filas)*100,2)
+  porcentaje_Ret <- round((contarRetweet$Cantidad_Re/total_filas$total_filas)*100,2)
+  porcentaje_loca <-round((contarLoc$Cantidad_Loca/total_filas$total_filas)*100,2)
+  porcentaje_des <-round((contarDesc$Cantidad_Des/total_filas$total_filas)*100,2)
+  Total = round((porcentaje_num+porcentaje_Ret+porcentaje_loca+porcentaje_des+porcentaje_des)/100,2)
+  
+  tabla <- matrix(c("Numeros en Usuario","Retweet","Localizacion","Descripcion",porcentaje_num,porcentaje_Ret,porcentaje_loca,porcentaje_des),ncol =2)
+  colnames(tabla) = c("Cantidad","Porcentaje")
+  tabla = as.data.frame(tabla)
+  write.csv(tabla, file = paste(CarpetaResultados,'PorcentajeParcialesBot.csv.csv',sep = "/"),row.names=FALSE)
+  
+  #Columnas con las probables cuentas
+  des <- as.data.frame(descripcion$cuenta)
+  loc <- as.data.frame(localizacion$cuenta)
+  cuen <- as.data.frame(cuenta$Cuentas)
+  ret <-as.data.frame( retweet$cuenta)
+  
+  #Apilamiento de todas la filas
+  tabla_final <- rbind.fill(des,loc,cuen,ret)
+  tabla_final <- as.data.frame(tabla_final)
+  
+  #Limpieza de las cuentas repetidas
+  final <- sqldf(" SELECT DISTINCT(descripcion$cuenta) FROM tabla_final")
+  numero_finalRT <- sqldf("SELECT count(*) 'Porcentaje de Bots' FROM final")
+  total <- round((numero_finalRT/total_filas)*100,2)
+  write.csv(total, file = paste(CarpetaResultados,'PorcentajeBotenBase.csv',sep = "/"),row.names=FALSE)
+  #Posibles cuentas Bots
+  write.csv(tabla_final$cuenta, paste(CarpetaResultados, 'Cuentas.csv', sep = "/"), row.names = FALSE)
+  
+  #---- NO RETWEET ----#
+  #NoRetweet <- sqldf("SELECT screen_name Cuenta, text
+  #FROM consulta WHERE is_retweet='0'")
+
+  i = i + 1
+  
+}
+
